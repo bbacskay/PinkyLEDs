fa83e7e097ab41b00df14243bb4dc2e1cf71ed70
Balázs Bácskay <bacskay.balazs@gmail.com>
2019-07-10 21:43:54 -0400

    Making config.h a template

--- PinkyLEDs/config_template.h
+++ PinkyLEDs/config_template.h
@@ -1,7 +1,7 @@
 #define DEVICE_NAME "PinkyLED1" // Unique name for this device
 #define USE_DISCOVERY // remove if you do not want to use auto-discovery
 #define USE_WHITE_BALANCE_FOR_SPEED // remove if you do not want to use Home Assistant's built in white balance slider for animation speed
-#define ENABLE_E131 // remove if you do not want to use E1.31
+//#define ENABLE_E131 // remove if you do not want to use E1.31
 
 #define wifi_ssid "yourSSID" // your WIFI SSID
 #define wifi_password "yourWiFiPassword" // your WIFI Password
@@ -11,12 +11,11 @@
 #define mqtt_password "MQTTPASSWORD" //enter your MQTT password
 #define mqtt_group_topic "cmnd/PinkyLEDs" // MQTT group topic to allow controlling multiple controllers with a single mqtt command
 
-#define DATA_PIN    D5 // the pin the LEDs are connected to. (on NodeMCU 1.0, FastLED will default to the D5 pin after throwing an error during compiling.)
 #define LED_TYPE    WS2812 // your LED type WS2812 / WS2811
 #define COLOR_ORDER GRB // your color order (normally: RGB for WS2811 | GRB for WS2812)
 #define NUM_LEDS    150 // number of LEDs in your strip
 
-// Different pin configurations for ESP32 and NodeMCU 1.0
+// Different pin configurations for ESP32 and ESP8266
 #if defined(ARDUINO_ESP8266_NODEMCU) || defined(ARDUINO_ESP8266_WEMOS_D1MINI)
   #define DATA_PIN          D1  // the pin the LEDs are connected to.
   #define POWER_BUTTON_PIN  D6  // pin the Power button is connected to.  If not using the button, leave it as it is

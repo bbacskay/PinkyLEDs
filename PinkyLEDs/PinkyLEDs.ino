7a03ffe46bd91033dd0dc67572ad3635b4814620
Balázs Bácskay <bacskay.balazs@gmail.com>
2019-10-07 19:11:40 -0400

    Calculating/setting the E1.31 ring buffer size based on NUM_LEDS

--- PinkyLEDs/PinkyLEDs.ino
+++ PinkyLEDs/PinkyLEDs.ino
@@ -36,6 +36,9 @@
   #if (UNIVERSE_START < 1) || (UNIVERSE_START > 63999)
     #error "UNIVERSE_START has to be 1 - 63999, check config.h"
   #endif
+
+  // Number of the required Universes
+  uint16_t universeReq;
 #endif
 
 #if defined(BRIGHTNESS_ENCODER_DT) || defined(SPEED_ENCODER_DT)
@@ -298,7 +301,7 @@ char message_buff[100];
 WiFiClient espClient; //this needs to be unique for each controller
 PubSubClient client(espClient); //this needs to be unique for each controller
 #ifdef ENABLE_E131
-  ESPAsyncE131 e131(2);
+  ESPAsyncE131* e131;
 #endif
 
 void setup() {
@@ -414,7 +417,18 @@ void setup() {
     Serial.println("OTA setup complete"); 
   #endif
   #ifdef ENABLE_E131
-    e131.begin(E131_UNICAST);
+    // Calculate the number of the required Universes
+    universeReq = (NUM_LEDS / 170);
+    if ( (NUM_LEDS % 170) > 0 )
+      {
+        universeReq++;
+      }
+    #ifdef DEBUG
+      Serial.printf("Required E1.31 Universes: %u\n", universeReq);
+    #endif
+
+    e131 = new ESPAsyncE131(universeReq);
+    e131->begin(E131_UNICAST);
   #endif
 }
 
@@ -641,22 +655,16 @@ void loop() {
     #ifdef BUILTIN_LED
       digitalWrite(BUILTIN_LED, LED_ON);
     #endif
-    if (!e131.isEmpty()) {
+    if (!e131->isEmpty()) {
       e131_packet_t packet;
-      e131.pull(&packet);     // Pull packet from ring buffer
+      e131->pull(&packet);     // Pull packet from ring buffer
       
-      // Calculate the required number of Universes
-      uint16_t universeReq = (NUM_LEDS / 170);
-      if ( (NUM_LEDS % 170) > 0 )
-      {
-        universeReq++;
-      }
-
       uint16_t universe = htons(packet.universe);
       uint16_t universeLast = universe + universeReq - 1;
       uint16_t maxChannels = htons(packet.property_value_count) - 1;
 
-      if ( universe >= UNIVERSE_START ) 
+      if ( ( universe >= UNIVERSE_START ) &&
+           ( universe <= universeLast   )    )
       {
         // Calculate LED range to update
         uint16_t firstLed = ((universe - UNIVERSE_START) * 170);
@@ -666,8 +674,8 @@ void loop() {
           Serial.printf("Universe %u / %u Channels | Packet#: %u / Errors: %u / FirstLed: %3u/ LastLed: %3u / CH1: %3u / CH2: %3u / CH3: %3u\n",
                     universe,                               // The Universe for this packet
                     maxChannels,                            // Start code is ignored, we're interested in dimmer data
-                    e131.stats.num_packets,                 // Packet counter
-                    e131.stats.packet_errors,               // Packet error counter
+                    e131->stats.num_packets,                 // Packet counter
+                    e131->stats.packet_errors,               // Packet error counter
                     firstLed,                               // First LED to update
                     lastLed-1,                              // Last LED to update
                     packet.property_values[1],              // Dimmer data for Channel 1
